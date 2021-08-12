878189c10e8a8c1ec039dc34a30e8b00f85eb00f
dhoniaridho <dhoniaridho@gmail.com>
2021-08-12 16:21:02 +0700

    add: project section

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,11 @@ import Cube from "../assets/images/cube.png";
 export default function Home() {
   return (
     <MainLayout>
-      <div className="bg-black place-items-center text-white h-screen--h-16 flex justify-between">
+      <section className="bg-black place-items-center text-white h-screen--h-16 flex justify-between">
         <div className="w-1/2 flex place-items-center flex-col">
           <div className="-mt-16">
             <div className="mt-4">
-            <h1>HI THERE👋, I’M</h1>
+            <h1 className="animate-">HI THERE👋, I’M</h1>
             <h1 className="text-5xl font-bold bg-gradient-to-tl from-pink-500 via-blue-400 to-indigo-400 bg-clip-text text-transparent inline">
               DHONI.
             </h1>
@@ -31,7 +31,42 @@ export default function Home() {
         <div className="w-1/2 flex justify-center place-items-center">
           <img draggable="false" className="w-1/2" src={Cube.src} alt="cube" />
         </div>
-      </div>
+      </section>
+      <section className="bg-black text-white px-20 py-10">
+          <div className="mb-10">
+            <p className="text-sm text-gray-400 mb-2">FEATURED PROJECT</p>
+            <h1 className="text-4xl">Stuff I’ve Worked On 📁</h1>
+          </div>
+          <div className="grid grid-cols-3 gap-5">
+              <div className="animate-pulse h-80 bg-gradient-to-bl from-pink-500 to-blue-500">
+            
+              </div>
+              <div className="animate-pulse h-80 bg-gradient-to-bl from-pink-500 to-blue-500">
+            
+              </div>
+              <div className="animate-pulse h-80 bg-gradient-to-bl from-pink-500 to-blue-500">
+            
+              </div>
+              <div className="animate-pulse h-80 bg-gradient-to-bl from-pink-500 to-blue-500">
+            
+              </div>
+              <div className="animate-pulse h-80 bg-gradient-to-bl from-pink-500 to-blue-500">
+            
+              </div>
+              <div className="animate-pulse h-80 bg-gradient-to-bl from-pink-500 to-blue-500">
+            
+              </div>
+              <div className="animate-pulse h-80 bg-gradient-to-bl from-pink-500 to-blue-500">
+            
+              </div>
+              <div className="animate-pulse h-80 bg-gradient-to-bl from-pink-500 to-blue-500">
+            
+              </div>
+              <div className="animate-pulse h-80 bg-gradient-to-bl from-pink-500 to-blue-500">
+            
+              </div>
+          </div>
+      </section>
     </MainLayout>
   );
 }
