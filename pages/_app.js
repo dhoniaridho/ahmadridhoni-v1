329974c434e80348e941287ae571052958c3e288
dhoniaridho <dhoniaridho@gmail.com>
2021-08-12 16:21:23 +0700

    add: project section

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
+import Router from 'next/router';
 import '../styles/globals.css'
+import NProgress from 'nprogress'; //nprogress module
+import 'nprogress/nprogress.css'; //styles of nprogress
 
 function MyApp({ Component, pageProps }) {
+
+  Router.events.on('routeChangeStart', () => NProgress.start()); Router.events.on('routeChangeComplete', () => NProgress.done()); Router.events.on('routeChangeError', () => NProgress.done());  
+
   return <Component {...pageProps} />
 }
 
