dddd9479faf27a1df147812bf0b5469606917c25
dhoniaridho <dhoniaridho@gmail.com>
2021-08-13 18:10:42 +0700

    add project

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,32 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import MainLayout from "~/components/layouts/main-layout";
 import Link from "next/link";
 import Cube from "../assets/images/cube.png";
 import Box from "../assets/images/box.png";
-import { useForm } from 'react-hook-form';
-import Alert from '~/components/ui/alert';
+import { useForm } from "react-hook-form";
+import Alert from "~/components/ui/alert";
 
 export default function Home() {
-
-  const [isSubmitting, setIsSubmitting] = useState(false)
-  const { register, handleSubmit, reset, formState: { errors }  } = useForm()
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
   const onSubmit = async (data) => {
-    setIsSubmitting(true)
+    setIsSubmitting(true);
     try {
-      setTimeout(()=>{
-        alert(JSON.stringify(data))
-        reset(data = '');
-        setIsSubmitting(false)
-      }, 3000)
+      setTimeout(() => {
+        alert(JSON.stringify(data));
+        reset((data = ""));
+        setIsSubmitting(false);
+      }, 3000);
     } catch (error) {
-      alert(error)
-      setIsSubmitting(false)
+      alert(error);
+      setIsSubmitting(false);
     }
-  }
+  };
 
   return (
     <MainLayout>
@@ -46,14 +50,23 @@ export default function Home() {
                   CONTACT ME
                 </a>
               </Link>
-              <a href="https://drive.google.com/file/d/1D1vFNxMR-RN8piUMNkm7gobULKibCOgI/view?usp=sharing" className="select-none py-3 mx-2 text-xs px-5 rounded-full">
-                  DOWNLOAD RESUME
-                </a>
+              <a
+                href="https://drive.google.com/file/d/1D1vFNxMR-RN8piUMNkm7gobULKibCOgI/view?usp=sharing"
+                className="select-none py-3 mx-2 text-xs px-5 rounded-full"
+              >
+                DOWNLOAD RESUME
+              </a>
             </div>
           </div>
         </div>
         <div className="order-1 md:order-2 my-10 md:w-1/2 flex justify-center place-items-center">
-          <img className="-mt-20" draggable="false" width="700" src={Box.src} alt="cube" />
+          <img
+            className="-mt-20"
+            draggable="false"
+            width="700"
+            src={Box.src}
+            alt="cube"
+          />
         </div>
       </section>
       <section className="bg-black text-white px-7 md:px-20 py-10">
@@ -62,7 +75,22 @@ export default function Home() {
           <h1 className="text-4xl">Stuff I’ve Worked On 📁</h1>
         </div>
         <div className="grid md:grid-cols-3 gap-5">
-          <div className="h-80 bg-gradient-to-bl from-pink-500 to-blue-500"></div>
+          <div className="group relative inline-flex hover:border-white border-2 focus:outline-none">
+            <div
+              role="button"
+              className="absolute inset-0 flex justify-between flex-col p-6 text-white text-center font-medium h-80 bg-gradient-to-bl from-pink-500 to-blue-500 transform transition-transform group-hover:-translate-y-2 group-hover:-translate-x-2 group-focus:-translate-y-1 group-focus:-translate-x-1"
+            >
+              <div className="flex-1 flex place-items-center">
+                <div className="mt-24">
+                  <h1 className="text-4xl font-semibold">Links</h1>
+                </div>
+              </div>
+              <div className="flex justify-center">
+                <p className="mx-1">REACT</p>
+                <p className="mx-1">TAILWIND</p>
+              </div>
+            </div>
+          </div>
           <div className="h-80 bg-gradient-to-bl from-pink-500 to-blue-500"></div>
           <div className="h-80 bg-gradient-to-bl from-pink-500 to-blue-500"></div>
           <div className="h-80 bg-gradient-to-bl from-pink-500 to-blue-500"></div>
@@ -95,14 +123,16 @@ export default function Home() {
                     </label>
                     <input
                       {...register("name", {
-                        required: true
+                        required: true,
                       })}
                       type="text"
                       id="name"
                       placeholder="Your Name"
                       className="py-2 text-black px-2 sm:w-96  focus:outline-none"
                     />
-                     {errors.name && <Alert type="danger">this filled is required</Alert>}
+                    {errors.name && (
+                      <Alert type="danger">this filled is required</Alert>
+                    )}
                   </div>
                   <div className="flex flex-col my-3">
                     <label className="text-sm mb-2" htmlFor="email">
@@ -110,14 +140,14 @@ export default function Home() {
                     </label>
                     <input
                       {...register("email", {
-                        required: true
+                        required: true,
                       })}
                       type="email"
                       id="email"
                       placeholder="Your Email"
                       className="py-2 text-black px-2 sm:w-96  focus:outline-none"
                     />
-                     {errors.email && <Alert>required</Alert>}
+                    {errors.email && <Alert>required</Alert>}
                   </div>
                   <div className="flex flex-col my-3">
                     <label className="text-sm mb-2" htmlFor="phone">
@@ -125,14 +155,14 @@ export default function Home() {
                     </label>
                     <input
                       {...register("subject", {
-                        required: true
+                        required: true,
                       })}
                       type="text"
                       id="phone"
                       placeholder="Your Phone Number"
                       className="py-2 text-black px-2 sm:w-96  focus:outline-none"
                     />
-                      {errors.subject && <Alert>required</Alert>}
+                    {errors.subject && <Alert>required</Alert>}
                   </div>
                   <div className="flex flex-col my-3">
                     <label className="text-sm mb-2" htmlFor="message">
@@ -140,23 +170,23 @@ export default function Home() {
                     </label>
                     <textarea
                       {...register("message", {
-                        required: true
+                        required: true,
                       })}
                       type="text"
                       id="message"
                       placeholder="Message"
                       className="py-2 text-black px-2 sm:w-96  focus:outline-none"
                     ></textarea>
-                     {errors.message && <Alert>required</Alert>}
+                    {errors.message && <Alert>required</Alert>}
                   </div>
-                <p className="my-2 text-sm">We respect your privacy.</p>
+                  <p className="my-2 text-sm">We respect your privacy.</p>
                 </fieldset>
                 <button
                   className="group relative h-12 inline-flex w-64 border border-white sm:w-56 focus:outline-none"
                   type="submit"
                 >
                   <span className="absolute inset-0 inline-flex items-center justify-center self-stretch px-6 text-white text-center font-medium bg-pink-600 ring-1 ring-pink-600 ring-offset-1 ring-offset-pink-600 transform transition-transform group-hover:-translate-y-2 group-hover:-translate-x-2 group-focus:-translate-y-1 group-focus:-translate-x-1">
-                    {isSubmitting ? 'loading' : 'Submit'}
+                    {isSubmitting ? "loading" : "Submit"}
                   </span>
                 </button>
               </form>
