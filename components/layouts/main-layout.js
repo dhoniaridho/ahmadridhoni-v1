66b353e4c990ef0cb845a512e00b0207b961609c
dhoniaridho <dhoniaridho@gmail.com>
2021-08-14 14:19:01 +0700

    add: add page title

--- components/layouts/main-layout.js
+++ components/layouts/main-layout.js
@@ -1,13 +1,20 @@
+import Head from "next/head";
 import Footer from "./footer";
 import Navigation from "./navigation";
 
 export default function MainLayout(props) {
-    const {children} = props
+    const {children, title} = props
     return(
         <>
+            <Head>
+                <title>{title}</title>
+            </Head>
            <Navigation />
            {children} 
            <Footer />
         </>
     )
+}
+MainLayout.defaultProps = {
+    title: 'Ahmad Ridhoni'
 }
