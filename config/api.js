e064b51c0aa16c5d7fff231b687f9e0ad717defb
Ahmad Ridhoni <dhoniaridho@gmail.com>
2021-08-15 16:43:11 +0700

    change base url

--- config/api.js
+++ config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: 'https://ahmadridhoni.vercel.app',
+    baseURL: 'https://dhoniaridho.netlify.app/',
     headers: {
         'Content-Type': 'application/json'
         }
